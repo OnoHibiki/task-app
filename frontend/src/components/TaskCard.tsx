452339481847a3c3695c57835588f267d2458217
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2026-01-10 23:26:33 +0900

    TaskCard.tsx作成

--- frontend/src/components/TaskCard.tsx
+++ frontend/src/components/TaskCard.tsx
@@ -22,5 +22,45 @@ export default function TaskCard({ task, onMove, onDelete }: Props) {
         if (task.status === "done") onMove(task.id, "doing");
     };
 
-     //続きはここから！！
+    const MoveRight = () => {
+        if (task.status === "todo") onMove(task.id, "doing");
+        if (task.status === "doing") onMove(task.id, "doing");
+    };
+
+    return(
+        <Paper variant="outlined" sx={{ p: 1.5, borderRadius: 2}}>
+            <Stack spacing={0.5}>
+                <Typography fontWeight={700}>{task.title}</Typography>
+                {task.description ? (
+                    <Typography variant="body2" color="textSecondary">
+                        {task.description}
+                    </Typography>
+                ) : null}
+
+                <Typography variant="caption" color="text.secondary">
+                    Status: {statusLabel[task.status]} / Priority: {task.priority}
+                    {task.dueDate ? ` / Due: ${task.dueDate}` : ""}
+                </Typography>
+
+                <Stack direction="row" spacing={1} sx={{ mt: 1 }}>
+                    <Button size="small" variant="outlined" disabled={!canMoveLeft} onClick={moveleft}>
+                        ←
+                    </Button>
+                    <Button size="small" variant="outlined" disabled={!canMoveRight} onClick={MoveRight}>
+                        →
+                    </Button>
+                    <Button
+                        size="small"
+                        color="error"
+                        variant="outlined"
+                        onClick={() => onDelete(task.id)}
+                        sx={{ ml: "auto"}}
+                    >
+                        Delete                        
+
+                    </Button>
+                </Stack>
+            </Stack>
+        </Paper>
+    )
 }
